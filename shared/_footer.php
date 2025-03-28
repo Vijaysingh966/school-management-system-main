04e9847ccf5aa171d69367954c180557db671b7c
vijaysingh966 <vijaysinghsisodiya966@gmail.com>
2025-03-28 18:54:34 +0530

    Project submitAttendence

---
+++ shared/_footer.php
@@ -0,0 +1,41 @@
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <!-- Insert website logo -->
+          <img src="images/logobg.png" alt="Website Logo">
+          <!-- Display visitor count -->
+          <p>LBS COLLEGE MANAGEMENT SYSTEM</p>
+          <!-- Display time zone -->
+          <p>312605, Pratapgarh(R.J),India </p>
+        </div>
+        
+        <div class="col-md-4">
+          <div class="footer-links">
+           
+          <p>follow us on</p>
+          </div>
+          <div class="social-icons">
+            <a href="#"><i class="fab fa-facebook-f facebook"></i></a>
+            <a href="#"><i class="fa-brands fa-x-twitter twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram instagram"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in linked-in"></i></a>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-4">
+        <div class="col-md-12">
+          <p>&copy; <?php echo date('Y'); ?> By <a href="https://github.com/vijaysingh966/" target="_blank">CodeWithSharp</a>. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+
+  <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+  <script src="js/bootstrap.bundle.js"></script>
+  <script src="./shared/app.js"></script>
+</body>
+
+</html>
---
+++ teacher_panel/partials/select_classes.php
@@ -0,0 +1,7 @@
+ 
+  <option value="12">BCA</option>
+ <option value="11">BSc</option>
+ <option value="10">PGDCA</option>
+ <option value="9">BA</option>
+ <option value="8">RSCIT</option>
+ <option value="7">MCA</option>
